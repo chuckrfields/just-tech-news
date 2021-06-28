6bf0b64a3186ddbf7ce69b5b4b858e3b6ff2b906
Chuck Fields <info@spacetechcorp.com>
2021-06-28 12:15:45 -0400

    adding handlebars and front-end

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
-
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes.js');
 
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 router.use((req, res) => {
     res.status(404).end();  // This is so if we make a request to any endpoint that doesn't exist, we'll receive a 404 error indicating we have requested an incorrect resource, another RESTful API practice.
