b2b99783d0598f176de3ef004a66256f781df8e6
Chuck Fields <info@spacetechcorp.com>
2021-06-29 16:57:51 -0400

    connect to jawsdb'

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,6 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-// // Create LOCAL db connection
-// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-//     host: 'localhost',
-//     dialect: 'mysql',
-//     port: 3306
-// } );
-
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
